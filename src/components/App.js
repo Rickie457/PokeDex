ddb78f195e7e251faa32d0c36dadc7119e4e8e6a
Rickie457 <rickie.tran7@gmail.com>
2023-05-11 17:31:30 -0400

    added data manipulation, updated fetch method

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import PokeAPI from "../pokeapi.png"
 import { typeButtons } from "./Functions"
 import { useEffect, useState, useRef } from "react"
 
+// All Pokemon types
 let types = [
   'Normal',
   'Fire',
@@ -27,44 +28,62 @@ let types = [
 ]
 
 function App() {
+  // All Pokemon from PokeAPI
   const [catalog, setCatalog] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // Filtered list everytime we search or click on filter buttons
   const [filtered, setFiltered] = useState([]);
+  // Search bar
   const [search, setSearch] = useState([]);
+  // Filter button types
   const [type, setTypeFilter] = useState("");
   const resetCatalog = useRef();
   const searchRender = useRef();
   let temp = [];
-  let i = 0;
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=811')
-    .then(res => res.json())
-    .then(json => {
-      json.results.forEach((pokemon) => {
-        fetchPokemonData(pokemon, i);
-        i+=1;
-      })
-      setFiltered(temp);
-      setCatalog(temp);
-    })
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=811');
+        const jsonData = await response.json();
+        
+        getAllPokeData(jsonData);
+        setCatalog(temp);
+        setFiltered(temp);
+      } catch (err) {
+        console.log('Error fetching data' + err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
   }, []);
 
-  const fetchPokemonData = (pokemon, i) => {
+  const getAllPokeData = (jsonData) => {
+    return jsonData.results.forEach((pokemon) => {
+      fetchPokemonData(pokemon);
+    });
+  }
+
+  const fetchPokemonData = (pokemon) => {
     let url = pokemon.url;
     fetch(url)
     .then(res => res.json())
     .then(json => {
-      temp.push(<PokeCard key={i} pokemon={json}/>)
+      temp.push(<PokeCard pokemon={json}/>)
     })
   }
 
   useEffect(() => {
-    let value = searchRender.current.value.toLowerCase();
-    if (value === '' || value.trim() === ' '){
-      setFiltered(catalog)
-    } else {
-      let temp = catalog.filter(components => components.props.pokemon.name.includes(value))
-      setFiltered(temp);
+    if(!isLoading){
+      let value = searchRender.current.value;
+      if (value === '' || value.trim() === ' '){
+        setFiltered(catalog)
+      } else {
+        let temp = catalog.filter(components => components.props.pokemon.name.includes(value))
+        setFiltered(temp);
+      }
     }
   }, [search])
 
@@ -85,35 +104,42 @@ function App() {
   }, [type])
 
   return (
-    <div className="min-w-screen font-pixel">
-      <div className="text-6xl flex justify-center align-center mb-12">
-        <img src={PokeAPI}></img>
-      </div>
-
-      <div className="w-1/2 mx-auto my-4">
-        <SearchBar refsearch={searchRender} onChange={e => {
-          setSearch(e.target.value);
-        }}/>
-      </div>
-        
-      <div className="w-1/2 mx-auto my-4">
-        <div>Filter By: </div>
-        <div className="flex flex-wrap">
-          {(() => {
-          let temp = [];
-          types.forEach((elem) => {
-            temp.push(typeButtons(elem, setTypeFilter, resetCatalog));
-          })
-          return temp;
-          })()}
+    <div>
+      {isLoading ? (
+        <div>Loading Data...</div>
+      ) : (
+      <div className="min-w-screen font-pixel">
+        <div className="text-6xl flex justify-center align-center mb-12">
+          <img src={PokeAPI}></img>
         </div>
-      </div>
 
-      <div className="w-4/6 flex flex-wrap justify-center align-center mx-auto my-0">
-        {filtered.length ? filtered : <span>Search for something!</span>}
-      </div>
-    </div> 
-  )
+        <div className="w-1/2 mx-auto my-4">
+          <SearchBar refsearch={searchRender} onChange={e => {
+            setSearch(e.target.value);
+          }}/>
+        </div>
+          
+        <div className="w-1/2 mx-auto my-4">
+          <div>Filter By: </div>
+          <div className="flex flex-wrap">
+            {(() => {
+            let temp = [];
+            types.forEach((elem) => {
+              temp.push(typeButtons(elem, setTypeFilter, resetCatalog));
+            })
+            return temp;
+            })()}
+          </div>
+        </div>
+
+        <div className="w-4/6 flex flex-wrap justify-center align-center mx-auto my-0">
+          {console.log(filtered.length)}
+          {filtered.length ? (<div>Loading...</div>) : (filtered)}
+        </div>
+      </div> 
+      )}
+    </div>
+  );
 }
 
 export default App
